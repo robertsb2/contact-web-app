7df03d26eca04e4c6d05201d3b02c7c14d69982c
Rob <rob12057@gmail.com>
2020-12-19 23:03:49 -0700

    Login page and demo functionality added

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './../../service/user.service';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  user = { email: '', password: '' };
+  currentUser$: Subscription;
+  hasLoginError: boolean;
+
+  constructor(
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.hasLoginError = false;
+
+    this.currentUser$ = this.userService._currentUser.subscribe(
+      data => {
+        if (data) {
+          this.router.navigate(['/contacts']);
+        }
+      },
+      err => {
+        console.log(err);
+      }
+    );
+
+    this.userService.authenticationErrorEvent.subscribe(
+      (err: any) => {
+        this.hasLoginError = true;
+        console.log(err);
+      });
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy(): void {
+    this.currentUser$.unsubscribe();
+  }
+
+
+  onClickSubmit(data: { email: string; passwd: string; }): void {
+    this.hasLoginError = false;
+    this.userService.login(data);
   }
 
 }
