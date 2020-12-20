5451a244462fb8079482153ea6124853838802be
Rob <rob12057@gmail.com>
2020-12-20 15:57:01 -0700

    Contacts page with dummy data loaded.
    Dummy data displayed in table, with search/filter functionality.

--- src/app/service/auth-guard.service.ts
+++ src/app/service/auth-guard.service.ts
@@ -1,15 +1,31 @@
+import { UserService } from './user.service';
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuardService implements CanActivate {
 
-  constructor(private router: Router) { }
+  private isAuthenticated = false;
+  private currentUser$: Subscription;
+  constructor(
+    private router: Router,
+    private userService: UserService
+  ) {
+    this.currentUser$ = this.userService._currentUser.subscribe(
+      data => {
+        if (data) {
+          this.isAuthenticated = true;
+        }
+      }
+    );
+  }
+
+
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean {
-    const isLoggedIn = true;
-    if (isLoggedIn) {
+    if (this.isAuthenticated) {
       return true;
     } else {
       this.router.navigate(['/login']);
--- src/app/service/user.service.ts
+++ src/app/service/user.service.ts
@@ -7,25 +7,40 @@ import { BehaviorSubject, Observable } from 'rxjs';
 })
 export class UserService {
 
-  private _currentUser$: BehaviorSubject<any> = new BehaviorSubject(null);
-  public _currentUser = this._currentUser$.asObservable();
+  private _currentUser$: BehaviorSubject<User | null>;
+  public _currentUser: Observable<User | null>;
   public authenticationErrorEvent = new EventEmitter();
+  devMode = true;
 
-  constructor() { }
+  constructor() {
+    const user = localStorage.getItem('currentUser');
+    console.log(user);
+    if (user) {
+      this._currentUser$ = new BehaviorSubject<User | null>(JSON.parse(user));
+    } else {
+      this._currentUser$ = new BehaviorSubject<User | null>(null);
+    }
+    this._currentUser = this._currentUser$.asObservable();
+  }
 
   login(data: { email: string; passwd: string; }): void {
 
     // For demo use only. Production application would use a back end to manage legitamate users
-    if (data.email && data.passwd) {
-      console.log('Logged In');
+    if (this.devMode) {
+      this._currentUser$.next(new User('jdos893j587hd', 'Demo User', 'email@email.com'));
+    } else if (data.email && data.passwd) {
       this._currentUser$.next(new User('jdos893j587hd', 'Demo User', data.email));
     } else {
       this.authenticationErrorEvent.emit('Invalid Credentials');
+      return;
     }
+
+    localStorage.setItem('currentUser', JSON.stringify(this._currentUser$.getValue()));
   }
 
   logout(): void {
     this._currentUser$.next(null);
+    localStorage.removeItem('currentUser');
   }
 
   /* I would normally include registration functionality,
